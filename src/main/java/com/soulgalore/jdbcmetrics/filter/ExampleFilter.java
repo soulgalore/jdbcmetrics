1680de975216b24b8bbc04c3695a2f5c60dc5ebd
Peter Hedenskog <peter@soulgalore.com>
2013-01-28 14:50:35 +0100

    added example filter

--- src/main/java/com/soulgalore/jdbcmetrics/filter/ExampleFilter.java
+++ src/main/java/com/soulgalore/jdbcmetrics/filter/ExampleFilter.java
@@ -1,6 +1,7 @@
 package com.soulgalore.jdbcmetrics.filter;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -9,25 +10,43 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
+import com.yammer.metrics.Metrics;
+import com.yammer.metrics.core.Meter;
+import com.yammer.metrics.core.Timer;
+import com.yammer.metrics.core.TimerContext;
+
 public class ExampleFilter implements Filter {
+
+	Meter getRequests;
+	Timer timer;
 
 	@Override
 	public void destroy() {
-		// TODO Auto-generated method stub
-		
+		getRequests.stop();
+		timer.stop();
 	}
 
 	@Override
-	public void doFilter(ServletRequest arg0, ServletResponse arg1,
-			FilterChain arg2) throws IOException, ServletException {
-		// TODO Auto-generated method stub
-		
+	public void doFilter(ServletRequest req, ServletResponse resp,
+			FilterChain chain) throws IOException, ServletException {
+
+		getRequests.mark();
+		final TimerContext context = timer.time();
+		try {
+			chain.doFilter(req, resp);
+		} finally {
+			context.stop();
+		}
+
 	}
 
 	@Override
 	public void init(FilterConfig arg0) throws ServletException {
-		// TODO Auto-generated method stub
-		
+		getRequests = Metrics.newMeter(ExampleFilter.class, "get-requests",
+				"requests", TimeUnit.SECONDS);
+		timer = Metrics.newTimer(ExampleFilter.class, "get-requests-timer",
+				TimeUnit.MILLISECONDS, TimeUnit.SECONDS);
+
 	}
 
 }
