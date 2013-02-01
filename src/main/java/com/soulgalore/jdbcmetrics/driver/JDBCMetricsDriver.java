465938aeab3a3ac5916854e4c6926ef42ff4d5b5
Magnus Lundberg <magnus.lundberg@gmail.com>
2013-02-01 13:34:05 +0100

    Driver, connection and statements wrappers

---
+++ src/main/java/com/soulgalore/jdbcmetrics/driver/JDBCMetricsDriver.java
@@ -0,0 +1,74 @@
+package com.soulgalore.jdbcmetrics.driver;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.DriverPropertyInfo;
+import java.sql.SQLException;
+import java.util.Enumeration;
+import java.util.Properties;
+
+public class JDBCMetricsDriver implements Driver {
+
+	static {
+		try {
+			DriverManager.registerDriver(new JDBCMetricsDriver());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public boolean acceptsURL(String url) throws SQLException {
+		return url.startsWith("jdbc:jdbcmetrics:");
+	}
+
+	@Override
+	public Connection connect(String url, Properties info) throws SQLException {
+		return new ConnectionWrapper(getDriver(url).connect(cleanUrl(url), info));
+	}
+
+	@Override
+	public int getMajorVersion() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMinorVersion() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public DriverPropertyInfo[] getPropertyInfo(String url, Properties info)
+			throws SQLException {
+		return getDriver(cleanUrl(url)).getPropertyInfo(url, info);
+	}
+
+	@Override
+	public boolean jdbcCompliant() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	protected Driver getDriver(String url) throws SQLException {
+		//TODO: "cache" the driver? on url?
+		if (acceptsURL(url)) {
+			url = cleanUrl(url);
+			Enumeration<Driver> drivers = DriverManager.getDrivers();
+			while (drivers.hasMoreElements()) {
+				Driver d = drivers.nextElement();
+				if (d.acceptsURL(url)) {
+					return d;
+				}
+			}
+		}
+		return null;
+	}
+
+	protected String cleanUrl(String url) {
+		return url.replaceFirst("\\:jdbcmetrics", "");
+	}
+}
