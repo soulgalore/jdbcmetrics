efbde67558fcf40e7a98ef5e664f737974a19147
Magnus Lundberg <magnus.lundberg@gmail.com>
2013-02-05 19:07:16 +0100

    More tests. Prepared and callable statement execute.

--- src/test/java/com/soulgalore/jdbcmetrics/driver/WhenQueryIsExecuted.java
+++ src/test/java/com/soulgalore/jdbcmetrics/driver/WhenQueryIsExecuted.java
@@ -3,7 +3,9 @@ package com.soulgalore.jdbcmetrics.driver;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -68,7 +70,48 @@ public class WhenQueryIsExecuted extends AbstractDriverTest {
 		assertThat(reads(), is(3));
 		assertThat(writes(), is(2));
 	}
+
+	@Test
+	public void clearBatchShouldResetCounters() throws SQLException {
+		statement.addBatch("SELECT 1");
+		statement.addBatch("INSERT 2");
+		statement.clearBatch();
+		assertThat(reads(), is(0));
+		assertThat(writes(), is(0));
+	}
 	
+	@Test
+	public void executePreparedStatementShouldIncreaseReadCounter() throws SQLException {
+		PreparedStatement pst = connection.prepareStatement("SELECT 1");
+		pst.execute();
+		assertThat(reads(), is(1));
+		assertThat(writes(), is(0));
+	}
+	
+	@Test
+	public void executePreparedStatementShouldIncreaseWriteCounter() throws SQLException {
+		PreparedStatement pst = connection.prepareStatement("INSERT 1");
+		pst.execute();
+		assertThat(reads(), is(0));
+		assertThat(writes(), is(1));
+	}
+
+	@Test
+	public void executeCallableStatementShouldIncreaseReadCounter() throws SQLException {
+		CallableStatement cst = connection.prepareCall("SELECT 1");
+		cst.execute();
+		assertThat(reads(), is(1));
+		assertThat(writes(), is(0));
+	}
+	
+	@Test
+	public void executeCallableStatementShouldIncreaseWriteCounter() throws SQLException {
+		CallableStatement cst = connection.prepareCall("INSERT 1");
+		cst.execute();
+		assertThat(reads(), is(0));
+		assertThat(writes(), is(1));
+	}
+
 	private int reads() {
 		return QueryThreadLocal.getNrOfQueries().getReads();
 	}
