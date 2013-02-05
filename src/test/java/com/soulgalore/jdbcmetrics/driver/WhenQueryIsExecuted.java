680bbf2e48945e1e3915d177eee43ca5d3d3df93
Magnus Lundberg <magnus.lundberg@gmail.com>
2013-02-05 12:17:20 +0100

    More tests

--- src/test/java/com/soulgalore/jdbcmetrics/driver/WhenQueryIsExecuted.java
+++ src/test/java/com/soulgalore/jdbcmetrics/driver/WhenQueryIsExecuted.java
@@ -37,13 +37,37 @@ public class WhenQueryIsExecuted extends AbstractDriverTest {
 	}
 	
 	@Test
+	public void executeShouldIncreaseWriteCounter() throws SQLException {
+		statement.execute("INSERT 1 (SELECT 2)");
+		assertThat(reads(), is(0));
+		assertThat(writes(), is(1));
+	}
+
+	@Test
+	public void executeQueryShouldIncreaseReadCounter() throws SQLException {
+		statement.executeQuery("SELECT 1");
+		assertThat(reads(), is(1));
+		assertThat(writes(), is(0));
+	}
+
+	@Test
 	public void executeUpdateShouldIncreaseWriteCounter() throws SQLException {
 		statement.executeUpdate("INSERT 1");
 		assertThat(reads(), is(0));
 		assertThat(writes(), is(1));
 	}
 
-	// implement rest
+	@Test
+	public void executeBatchShouldIncreaseCounters() throws SQLException {
+		statement.addBatch("SELECT 1");
+		statement.addBatch("INSERT 2");
+		statement.addBatch("SELECT 3");
+		statement.addBatch("SELECT 4");
+		statement.addBatch("INSERT 5");
+		statement.executeBatch();
+		assertThat(reads(), is(3));
+		assertThat(writes(), is(2));
+	}
 	
 	private int reads() {
 		return QueryThreadLocal.getNrOfQueries().getReads();
