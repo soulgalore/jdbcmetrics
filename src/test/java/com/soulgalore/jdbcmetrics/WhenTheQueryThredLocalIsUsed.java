afd34807616c75ac13c858b480363e0dbd896ed7
Peter Hedenskog <peter@soulgalore.com>
2013-02-06 13:51:38 +0100

    first commit

---
+++ src/test/java/com/soulgalore/jdbcmetrics/WhenTheQueryThredLocalIsUsed.java
@@ -0,0 +1,35 @@
+package com.soulgalore.jdbcmetrics;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+public class WhenTheQueryThredLocalIsUsed {
+
+
+
+	@Test
+	public void theNumberOfReadsShouldBeUpdated() {
+		QueryThreadLocal.init();
+		QueryThreadLocal.addRead();
+		try {
+		assertThat(QueryThreadLocal.getNrOfQueries().getReads(),is(1));
+		}
+		finally {
+		QueryThreadLocal.removeNrOfQueries();
+		}
+	}
+
+	@Test
+	public void theNumberOfWritesShouldBeUpdated() {
+		QueryThreadLocal.init();
+		QueryThreadLocal.addWrite();
+		try {
+		assertThat(QueryThreadLocal.getNrOfQueries().getWrites(),is(1));
+		}
+		finally {
+		QueryThreadLocal.removeNrOfQueries();
+		}
+	}
+}
